d78d2a233c97d0ccbb9a9ce253ef1f4a4e0917b9
Florian Frank <flori@ping.de>
2011-01-27 02:16:33 +0100

    added string numeral

--- lib/more_math.rb
+++ lib/more_math.rb
@@ -3,9 +3,17 @@ module MoreMath
     Infinity = 1.0 / 0      # Refers to floating point infinity.
   end
 
-  Dir.chdir(File.join(File.dirname(__FILE__), 'more_math', '..')) do
-    Dir['**/*.rb'].each do |filename|
-      require filename.gsub(/\.rb\Z/, '')
-    end
-  end
+  require 'more_math/cantor_pairing_function'
+  require 'more_math/constants/functions_constants'
+  require 'more_math/continued_fraction'
+  require 'more_math/distributions'
+  require 'more_math/exceptions'
+  require 'more_math/functions'
+  require 'more_math/histogram'
+  require 'more_math/linear_regression'
+  require 'more_math/newton_bisection'
+  require 'more_math/numberify_string_function'
+  require 'more_math/sequence'
+  require 'more_math/string_numeral'
+  require 'more_math/version'
 end
---
+++ lib/more_math/string_numeral.rb
@@ -0,0 +1,145 @@
+require 'more_math'
+
+module MoreMath
+  class StringNumeral
+    include ::MoreMath::NumberifyStringFunction
+
+    def self.from(object, alphabet = 'a'..'z')
+      if object.respond_to?(:to_str)
+        StringNumeral.from_string(object.to_str, alphabet)
+      elsif object.respond_to?(:to_int)
+        StringNumeral.from_number(object.to_int, alphabet)
+      else
+        StringNumeral.from_string(object.to_s, alphabet)
+      end
+    end
+
+    def self.from_string(string, alphabet)
+      new string, nil, alphabet
+    end
+
+    def self.from_number(number, alphabet)
+      new nil, number, alphabet
+    end
+
+    def initialize(string, number, alphabet)
+      @alphabet = NumberifyStringFunction.convert_alphabet(alphabet).freeze
+      if string
+        @string = string.to_s
+        string.each_char.each do |c|
+          @alphabet.include?(c) or raise ArgumentError,
+            "illegal character #{c.inspect} in #{@string.inspect} for alphabet #{@alphabet.inspect}"
+        end
+      elsif number
+        @number = number.to_i
+      end
+    end
+    private_class_method :new
+
+    def number
+      @number ||= numberify_string(@string, @alphabet)
+    end
+    alias to_i number
+
+    def string
+      @string ||= stringify_number(@number, @alphabet).freeze
+    end
+
+    alias to_s string
+
+    def inspect
+      "#<#{self.class}: #{string.inspect} #{number.inspect}>"
+    end
+
+    attr_reader :alphabet
+
+    def coerce(other)
+      [ number, naturalize(other) ]
+    end
+
+    def *(other)
+      self.class.from_number(number * naturalize(other), @alphabet)
+    end
+
+    def +(other)
+      self.class.from_number(number + naturalize(other), @alphabet)
+    end
+
+    def -(other)
+      self.class.from_number(naturalize(number - other), @alphabet)
+    end
+
+    def /(other)
+      self.class.from_number((number / naturalize(other)), @alphabet)
+    end
+
+    def %(other)
+      self.class.from_number((number % naturalize(other)), @alphabet)
+    end
+
+    def **(other)
+      self.class.from_number(number ** naturalize(other), @alphabet)
+    end
+
+    def <<(other)
+      self.class.from_number(number << naturalize(other), @alphabet)
+    end
+
+    def >>(other)
+      self.class.from_number(number >> naturalize(other), @alphabet)
+    end
+
+    def ^(other)
+      self.class.from_number(number ^ naturalize(other), @alphabet)
+    end
+
+    def &(other)
+      self.class.from_number(number & naturalize(other), @alphabet)
+    end
+
+    def |(other)
+      self.class.from_number(number | naturalize(other), @alphabet)
+    end
+
+    def [](other)
+      self.class.from_number(number[other.to_i], @alphabet)
+    end
+
+    def succ
+      self.class.from_number(number + 1, @alphabet)
+    end
+
+    def succ!
+      @number += 1
+      @string = nil
+      self
+    end
+
+    def pred
+      self.class.from_number(naturalize(number - 1), @alphabet)
+    end
+
+    def pred!
+      @number = naturalize(@number - 1)
+      @string = nil
+      self
+    end
+
+    private
+
+    def naturalize(number)
+      number = number.to_i
+      number < 0 ? 0 : number
+    end
+
+    module Functions
+      def StringNumeral(other, alphabet = 'a'..'z')
+        ::MoreMath::StringNumeral.from(other, alphabet)
+      end
+    end
+  end
+
+  class ::Object
+    include StringNumeral::Functions
+  end
+end
