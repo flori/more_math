b7ecc5df374bc6ea0e1b9527d01f6ae70d45b7d5
Florian Frank <flori@ping.de>
2016-10-20 16:12:53 +0200

    Bump version

--- lib/more_math/version.rb
+++ lib/more_math/version.rb
@@ -1,6 +1,6 @@
 module MoreMath
   # MoreMath version
-  VERSION         = '0.2.1'
+  VERSION         = '0.3.0'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
